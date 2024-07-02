f27af42901ae65424e5a1e1d9d3fd55e4521cf22
KoplarKoprol <putu.moreno298@gmail.com>
2024-07-02 23:39:48 +0800

    mod nav & view messages box. add ganti nama

--- js/skrip.js
+++ js/skrip.js
@@ -1,7 +1,38 @@
 function gantiNama() {
-    let nama = prompt("Infokan nama kamu", "acumalaka");
-    document.getElementById("nama").innerHTML = nama;
+let nama = prompt("Infokan nama kamu", "acumalaka");
+document.getElementById("nama").innerHTML = nama;
 }
-let tombol = document.getElementById("tombol").addEventListener("click", function () { 
-    gantiNama() 
+let tombol = document.getElementById("tombol").addEventListener("click", function () {
+    gantiNama()
 });
+
+function validateForm() {
+    const nama = document.getElementById("nama-lengkap").value;
+    const tanggalLahir = document.getElementById("tgllhr").value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const messages = document.getElementById("pesan").value;
+
+    if (nama === "" || tanggalLahir === "" || gender === "" || messages === "") {
+        alert("Isilah kolom yang kosong!");
+        return false;
+    } else {
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        });
+
+        // Update the view-form section
+        document.getElementById("nama-lengkap-pengirim").innerText = nama;
+        document.getElementById("tanggal-lahir-pengirim").innerText = tanggalLahir;
+        document.getElementById("gender-pengirim").innerText = gender;
+        document.getElementById("pesan-pengirim").innerText = messages;
+        document.getElementById("tanggal-submit").innerText = formattedDate;
+
+        return false;
+    }
+}
